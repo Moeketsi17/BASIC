cfeb5b1bb5fdf3f312023f3d9504866bdf7240d6
moeketsi <raymondkxng17@gmail.com>
2022-05-19 13:47:42 +0200

    commit

--- index.js
+++ index.js
@@ -30,14 +30,17 @@ slider.addEventListener('mousedown', (e) => {
     startX = e.pageX - slider.offsetLeft;
     scrollLeft = slider.scrollLeft;
 });
+
 slider.addEventListener('mouseleave', () => {
     isDown = false;
     slider.classList.remove('active');
 });
+
 slider.addEventListener('mouseup', () => {
     isDown = false;
     slider.classList.remove('active');
 });
+
 slider.addEventListener('mousemove', (e) => {
     if(!isDown) return;
     e.preventDefault();
