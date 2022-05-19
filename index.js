4213a12e380279e885812aaf46ccf3fb1beeb796
moeketsi <raymondkxng17@gmail.com>
2022-05-19 12:58:33 +0200

    commit

--- index.js
+++ index.js
@@ -18,4 +18,38 @@ function cursor(e) {
 
 
 
+// work mouse grab scroll
+const slider = document.querySelector('.featured-company-container');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(walk);
+});
+
+
+
+
+
+
 
