4a4f69146f5bf92a3ca2d44eb7b9ec1789568fec
moeketsi <raymondkxng17@gmail.com>
2022-05-20 13:24:19 +0200

    commit

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // header mouse
-let mouseCursor = document.querySelectorAll('.header-cursor');
+let mouseCursor = document.querySelector(".header-cursor");
 
-document.addEventListener('mousemove', cursor)
-function cursor(e) {
-    let x = e.clientX;
-    let y = e.clientY;
-
-    mouseCursor.style.left = `${x}px`;
-    mouseCursor.style.top = `${y}px`;
+window.addEventListener("mousemove", cursor)
+function cursor(e){
+    mouseCursor.style.top = e.pageY + "px";
+    mouseCursor.style.left = e.pageX + "px";
 }
+
 
 
 
