4613669277e053743da77f57d1594c78df333445
moeketsi <raymondkxng17@gmail.com>
2022-06-14 14:03:40 +0200

    commit

--- index.js
+++ index.js
@@ -128,7 +128,8 @@ burger.addEventListener("click", () => {
 
 
 // work mouse grab scroll
-let slider = document.querySelector('.featured-company-container', '.initiatives-container ');
+let slider = document.querySelector('.featured-company-container','.initiatives-container');
+let slider2 = document.querySelector('.initiatives-container');
 let isDown = false;
 let startX;
 let scrollLeft;
@@ -156,6 +157,32 @@ slider.addEventListener('mousemove', (e) => {
     const x = e.pageX - slider.offsetLeft;
     const walk = (x - startX) * 3; //scroll-fast
     slider.scrollLeft = scrollLeft - walk;
+    // console.log(walk);
+});
+
+slider2.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider2.classList.add('active');
+    startX = e.pageX - slider2.offsetLeft;
+    scrollLeft = slider2.scrollLeft;
+});
+
+slider2.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider2.classList.remove('active');
+});
+
+slider2.addEventListener('mouseup', () => {
+    isDown = false;
+    slider2.classList.remove('active');
+});
+
+slider2.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider2.offsetLeft;
+    const walk = (x - startX) * 3; //scroll-fast
+    slider2.scrollLeft = scrollLeft - walk;
     // console.log(walk);
 });
 
